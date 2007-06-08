628a42d37b3ebe98eb722ac02e480f450c20eefc
tkirby <tkirby@f31480fa-141b-0410-8f85-e1b837e7f62b>
2007-06-08 05:41:40 +0000

    Creating branch 0.5.1 from revision 463 of trunk

---
+++ ffmpeg-php-0.5.1/php_ffmpeg.h
@@ -0,0 +1,46 @@
+#ifndef PHP_FFMPEG_H
+#define PHP_FFMPEG_H
+
+#define SAFE_STRING(s) ((s)?(s):"")
+
+#ifndef safe_emalloc
+    # define safe_emalloc(a,b,c) emalloc((a)*(b)+(c))
+#endif
+
+
+extern zend_module_entry ffmpeg_module_entry;
+#define phpext_ffmpeg_ptr &ffmpeg_module_entry
+
+#ifdef PHP_WIN32
+#define PHP_FFMPEG_API __declspec(dllexport)
+#else
+#define PHP_FFMPEG_API
+#endif
+
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
+PHP_MINIT_FUNCTION(ffmpeg);
+PHP_MSHUTDOWN_FUNCTION(ffmpeg);
+PHP_RINIT_FUNCTION(ffmpeg);
+PHP_RSHUTDOWN_FUNCTION(ffmpeg);
+PHP_MINFO_FUNCTION(ffmpeg);
+
+
+#ifdef ZTS
+#define FFMPEG_G(v) TSRMG(ffmpeg_globals_id, zend_ffmpeg_globals *, v)
+#else
+#define FFMPEG_G(v) (ffmpeg_globals.v)
+#endif
+
+#endif	/* PHP_FFMPEG_H */
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * indent-tabs-mode: t
+ * End:
+ */
---
+++ ffmpeg-php-0.5.1/tests/test_ffmpeg.php
@@ -0,0 +1,97 @@
+<?
+/*
+ * This test script is not part of the automatic regression tests. It serves
+ * as a simple manual test script and an example of the syntax for calling
+ * the ffmpeg-php functions
+ * 
+ * To run it from the command line type 'php -q ffmpeg_test.php 'or from a 
+ * browser * copy this file into your web root and point your browser at it.
+ */
+
+$extension = "ffmpeg";
+$extension_soname = $extension . "." . PHP_SHLIB_SUFFIX;
+$extension_fullname = PHP_EXTENSION_DIR . "/" . $extension_soname;
+
+// load extension
+if (!extension_loaded($extension)) {
+    dl($extension_soname) or die("Can't load extension $extension_fullname\n");
+}
+
+if (php_sapi_name() != 'cgi') {
+    echo '<pre>';
+}
+
+printf("ffmpeg-php version string: %s\n", FFMPEG_PHP_VERSION_STRING);
+printf("libavcodec build number: %d\n", LIBAVCODEC_BUILD_NUMBER);
+printf("libavcodec version number: %d\n", LIBAVCODEC_VERSION_NUMBER);
+printf("libavcodec build number: %d\n", LIBAVCODEC_BUILD_NUMBER);
+
+print_class_methods("ffmpeg_movie");
+print_class_methods("ffmpeg_frame");
+print_class_methods("ffmpeg_animated_gif");
+
+// get an array for movies from the test media directory 
+$movies = getDirFiles(dirname(__FILE__) . '/test_media');
+
+echo "--------------------\n\n";
+foreach($movies as $movie) {        
+    $mov = new ffmpeg_movie($movie);
+    printf("file name = %s\n", $mov->getFileName());
+    printf("duration = %s seconds\n", $mov->getDuration());
+    printf("frame count = %s\n", $mov->getFrameCount());
+    printf("frame rate = %0.3f fps\n", $mov->getFrameRate());
+    printf("comment = %s\n", $mov->getComment());
+    printf("title = %s\n", $mov->getTitle());
+    printf("author = %s\n", $mov->getAuthor());
+    printf("copyright = %s\n", $mov->getCopyright());
+    printf("frame height = %d pixels\n", $mov->getFrameHeight());
+    printf("frame width = %d pixels\n", $mov->getFrameWidth());
+    printf("has audio = %s\n", $mov->hasAudio() == 0 ? 'No' : 'Yes');
+    printf("get pixel format = %s\n", $mov->getPixelFormat());
+    printf("get pixel aspect ratio = %s\n", $mov->getPixelAspectRatio());
+    printf("get video codec = %s\n", $mov->getVideoCodec());
+    printf("get audio codec = %s\n", $mov->getAudioCodec());
+    printf("get audio channels = %s\n", $mov->getAudioChannels());
+    printf("get bit rate = %d\n", $mov->getBitRate());
+    printf("get audio bit rate = %d\n", $mov->getAudioBitRate());
+    printf("get audio sample rate = %d \n", $mov->getAudioSampleRate());
+    printf("get video bit rate = %d\n", $mov->getVideoBitRate());
+    printf("get frame = %s\n", is_object($mov->getFrame(10)) ? 'true' : 'false');
+    printf("get frame number = %d\n", $mov->getFrameNumber());
+    echo "\n--------------------\n\n";
+}
+
+if (php_sapi_name() != 'cgi') {
+    echo '</pre>';
+}
+
+/* FUNCTIONS */
+function print_class_methods($class) {
+    echo "\nMethods available in class '$class':\n";
+    $methods = get_class_methods($class);
+    if (is_array($methods)) {
+        foreach($methods as $method) {
+            echo $method . "\n";
+        }
+    } else {
+        echo "No Methods Defined\n";
+    }
+}
+
+function getDirFiles($dirPath)
+{
+    if ($handle = opendir($dirPath))
+    {
+        while (false !== ($file = readdir($handle))) {
+            $fullpath = $dirPath . '/' . $file;
+            if (!is_dir($fullpath) && $file != "CVS" && $file != "." && $file != "..")
+                $filesArr[] = trim($fullpath);
+        }
+        closedir($handle);
+    } 
+
+    return $filesArr;   
+}
+
+
+?>
